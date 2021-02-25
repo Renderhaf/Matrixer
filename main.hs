20a125eb503c42f24eb48c119b3bae9f085928b1
Renderhaf <dvir.akerman@gmail.com>
2021-02-25 18:42:39 +0200

    Added example matrix file

--- main.hs
+++ main.hs
@@ -5,4 +5,4 @@ import Solver
 import Parser
 import Helpers
 
-main = ( applyRowOp (Add 0 1 2) <$$> stdinParser ) >>= print
+main = print =<< ( applyRowOp (Add 0 1 2) <$$> stdinParser )
--- solver.hs
+++ solver.hs
@@ -2,12 +2,12 @@ module Solver where
 
 import Types
 
--- Gets a matrix, returns the row operation requiered to get the matrix
--- one step forward in the gaussian elimination proccess
--- getClosestRowOp :: Matrix -> RowOperation
--- getClosestRowOp = 
+-- | Gets a matrix, returns the row operation requiered to get the matrix
+-- | one step forward in the gaussian elimination proccess
+getClosestRowOp :: Matrix -> RowOperation
+getClosestRowOp _ = undefined
 
--- Takes a row operation and applies it to matrix
+-- | Takes a row operation and applies it to matrix
 applyRowOp :: RowOperation -> Matrix -> Matrix
 applyRowOp (Multiply a s) mat = before ++ [map (*s) (mat !! a)] ++ after 
     where before = take a mat
@@ -25,8 +25,8 @@ applyRowOp (Add a s c) mat = before ++ [newrow] ++ after
           after  = drop (c+1) mat
           newrow = map (\t -> (fst t) + s * (snd t)) (zip (mat !! c) (mat !! a))
 
--- Takes a matrix, returns the row operations required to get the matrix to
--- row echlon form
--- solveMatrix :: Matrix -> [RowOperation]
--- solveMatrix mat = [currentOp] ++ solveMatrix (applyRowOp currentOp mat)
---         where currentOp = getClosestRowOp mat
+-- | Takes a matrix, returns the row operations required to get the matrix to
+-- | row echlon form
+solveMatrix :: Matrix -> [RowOperation]
+solveMatrix mat = [currentOp] ++ solveMatrix (applyRowOp currentOp mat)
+        where currentOp = getClosestRowOp mat
